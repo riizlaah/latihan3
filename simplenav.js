479fd0a0aac1eb6a3d6eba2883e1de0044a3b5d7
riizlaah <100104632+riizlaah@users.noreply.github.com>
2025-11-03 13:17:53 +0700

    add simple navbar

---
+++ simplenav.js
@@ -0,0 +1,12 @@
+function createElement(tag, prop = {}) {
+    return Object.assign(document.createElement(tag), prop);
+}
+const nav = createElement('nav', { className: 'closeable', innerHTML: `<a href="index.html">1</a><a href="index2.html">2</a><a href="index3.html">3</a><a href="index4.html">4</a>`})
+document.body.prepend(nav);
+document.addEventListener('mousemove', (event) => {
+    if(event.clientY < 100) {
+        nav.style.display = 'flex';
+    } else {
+        nav.style.display = 'none';
+    }
+});
