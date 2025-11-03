4dde1c9594c584d33cf493c56826f629eaa1b67e
riizlaah <100104632+riizlaah@users.noreply.github.com>
2025-11-03 13:26:32 +0700

    adjusting the navbar

--- simplenav.js
+++ simplenav.js
@@ -4,7 +4,7 @@ function createElement(tag, prop = {}) {
 const nav = createElement('nav', { className: 'closeable', innerHTML: `<a href="index.html">1</a><a href="index2.html">2</a><a href="index3.html">3</a><a href="index4.html">4</a>`})
 document.body.prepend(nav);
 document.addEventListener('mousemove', (event) => {
-    if(event.clientY < 100) {
+    if(event.clientY < 75) {
         nav.style.display = 'flex';
     } else {
         nav.style.display = 'none';
